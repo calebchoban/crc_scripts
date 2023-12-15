Small fixes to IO to correct units. Added more simple plots to investigate grain size distributions and some analytical functions for testing.

src/crc_scripts/config.py
@@ -54,7 +54,6 @@ mpl.rcParams['hatch.linewidth'] = BASE_ELINEWIDTH
 UnitLength_in_cm            = 3.085678e21   # 1.0 kpc/h
 UnitMass_in_g               = 1.989e43  	# 1.0e10 solar masses/h
 UnitMass_in_Msolar			= UnitMass_in_g / 1.989E33
-Grams_to_Msolar 			= 5.02785e-34
 UnitVelocity_in_cm_per_s    = 1.0e5   	    # 1 km/sec
 UnitTime_in_s 				= UnitLength_in_cm / UnitVelocity_in_cm_per_s # 0.978 Gyr/h
 UnitTime_in_Gyr 			= UnitTime_in_s /1e9/365./24./3600.
@@ -72,6 +71,7 @@ km_per_kpc					= 3.086E16
 sec_per_Gyr					= 3.16E16
 cm_to_um					= 1E4
 Ergs_per_joule				= 1E7
+grams_to_Msolar				= 5.02785e-34
 SOLAR_GAL_RADIUS			= 8 # kpc
 HUBBLE 						= 0.702
 OMEGA_MATTER 				= 0.272
@@ -99,6 +99,7 @@ SIL_ELEM_INDEX				= np.array([4,6,7,10]) # O,Mg,Si,Fe
 # number of atoms that make up one formula unit of silicate dust assuming an olivine, pyroxene mixture
 # with olivine fraction of 0.32 and Mg fraction of 0.8
 SIL_NUM_ATOMS				= np.array([3.631,1.06,1.,0.571]) # O,Mg,Si,Fe
+DUST_BULK_DENS				= np.array([3.13,2.25,3.21,7.86]) # silicates, carbonaceous, SiC, metallic iron (g/cm^-3)
 
 DUST_SPECIES				= ['Silicates','Carbon','Iron','O Reservoir','SiC','Iron Inclusions']
 DUST_SPECIES_SIL_CARB		= ['Silicates+','Carbon']
@@ -149,7 +150,7 @@ PROP_INFO  				= {'fH2': [r'$f_{\rm H_2}$', 													[0., 1.], 		False],
 						'M_dust': [r'$M_{\rm dust}\;[M_{\odot}]$',										[1E4,1E9],		True],
 							'nH': [r'$n_{\rm H}$ [cm$^{-3}$]', 											[3E-2, 0.9E3],  True],
 					'nH_neutral': [r'$n_{\rm H,neutral}$ [cm$^{-3}$]', 									[3E-2, 0.9E3],  True],
-							 'T': [r'T [K]', 															[1.1E1,0.9E5],  True],
+							 'T': [r'T [K]', 															[1.1E1,0.9E7],  True],
 							 'Z': [r'Z [Z$_{\odot}$]', 													[1.1E-3,5E0], 	True],
 					 'stellar_Z': [r'Z_{\rm star} [Z$_{\odot}$]', 										[1.1E-3,5E0], 	True],
 						   'Z_O': ['[O/H]', 															[1.1E-3,5E0], 	True],
@@ -203,7 +204,7 @@ PROP_INFO  				= {'fH2': [r'$f_{\rm H_2}$', 													[0., 1.], 		False],
 					'cum_wind_E': [r'Cum. Energy $E_{\rm inj,cum}}/M_{\star}$ [erg $M_{\star}^{-1}$]',	[6E17,5E18],	True],
 **dict.fromkeys(['lambda','wavelength'], [r'$\lambda \, [\mu m]$', 										[6E-2,1E3], 	True]),
 			   			   'SED': [r'$\lambda L_{\lambda} \,[L_{\odot}]$',								[1E8,2E11],		True],
-					'grain_size': [r'a ($\mu m$)',														[1E-3,10],		True],
+					'grain_size': [r'a ($\mu m$)',														[7E-4,10],		True],
 **dict.fromkeys(['dn/da','sil_dn/da','carb_dn/da','SiC_dn/da','iron_dn/da'],
 								  [r'$\frac{\partial n}{\partial a}$',									[1E-10,1E4],	True]),
 **dict.fromkeys(['dm/da','sil_dm/da','carb_dm/da','SiC_dm/da','iron_dm/da'],

src/crc_scripts/io/particle.py
@@ -168,7 +168,7 @@ class Particle:
                             # THIS WILL PROBABLY CHANGE TO BE MASS INSTEAD OF SLOPE FOR FINAL RUNS
                             grain_bin_nums[nL:nR] = np.power(10,grp['DustBinNumbers'][...].reshape((npart, sp.Flag_DustSpecies,sp.Flag_GrainSizeBins)),dtype='double')
                             grain_bin_slopes[nL:nR] = grp['DustBinSlopes'][...].reshape((npart, sp.Flag_DustSpecies,sp.Flag_GrainSizeBins))
-                            grain_bin_slopes[nL:nR] = np.sign(grain_bin_slopes[nL:nR])*np.power(10,np.abs(grain_bin_slopes[nL:nR]),dtype='double')
+                            grain_bin_slopes[nL:nR] = np.sign(grain_bin_slopes[nL:nR])*np.power(10,np.abs(grain_bin_slopes[nL:nR]),dtype='double') / (config.cm_to_um*config.cm_to_um)
                             # No dust grains are denoted by -1 in snapshots
                             no_dust = grain_bin_nums==0.1 
                             grain_bin_nums[no_dust] = 0;grain_bin_slopes[no_dust] = 0;
@@ -221,6 +221,9 @@ class Particle:
                     self.fdense = fdense
                     self.CinCO = CinCO
                     if (sp.Flag_GrainSizeBins):
+                        # Since dn/da is normalized to to the dust mass in the code, need to multiply by h factor
+                        grain_bin_nums /= hinv
+                        grain_bin_slopes /= hinv
                         self.grain_bin_nums = grain_bin_nums
                         self.grain_bin_slopes = grain_bin_slopes
             if (sp.Flag_Sfr):

src/crc_scripts/plot_functions.py
@@ -2208,10 +2208,10 @@ def DZ_var_in_pixel(gas, header, center_list, r_max_list, Lz_list=None, \
 
 
 
-def plot_grain_size_bins(snaps, y_props='grain_num_prop', mask_criterias=None, labels=None, foutname='grain_size_dist.png', 
+def plot_binned_grain_distribution(snaps, y_props='dn/da', mask_criterias=None, labels=None, foutname='binned_grain_size_dist.png', 
 						 std_bars=True, style='color-linestyle'):
 	"""
-	Plots the between two properties for multiple simulations/snapshots
+	Plots grain size distribution binned across gas particles.
 
 	Parameters
 	----------
@@ -2259,6 +2259,94 @@ def plot_grain_size_bins(snaps, y_props='grain_num_prop', mask_criterias=None, l
 			axis.plot(x_vals, y_mean, label=label, linestyle=linestyles[j], color=colors[j], linewidth=linewidths[j], zorder=3)
 			if std_bars:
 				axis.fill_between(x_vals, y_std[:,0], y_std[:,1], alpha = 0.3, color=colors[j], zorder=1)
+
+		
+	axis.legend(loc='best', fontsize=config.SMALL_FONT, frameon=False)
+
+	plt.tight_layout()
+	plt.savefig(foutname)
+	plt.close()
+
+	return
+
+
+
+def plot_grain_size_bins(snap, y_prop='dn/da', particle_ids=None, foutname='grain_size_dist.png', 
+						 style='color-linestyle'):
+	"""
+	Plots the full grain size distribution for a few select particles. Useful for testing.
+
+	Parameters
+	----------
+	snaps : list
+	    List of snapshots to plot
+	y_prop: string
+		Choose whether to plot grain size probability or grain mass propability and which dust species
+	particle_ids: list
+		List of particle ids to plot. If none are given then chose a particle from each of the ISM phases.
+	labels : list, optional
+		List of labels for each snapshot
+	foutname: string, optional
+		Name of file to be saved
+	style : string
+		Plotting style when plotting multiple data sets
+		'color-line' - gives different color and linestyles to each data set
+		'size' - make all lines solid black but with varying line thickness
+	Returns
+	-------
+	None
+
+	"""
+
+	G = snap.loadpart(0)
+	IDs = G.id
+
+	if particle_ids is None:
+		# Get plot stylization
+		linewidths,colors,linestyles = plt_set.setup_plot_style(3, style=style)
+		T = G.get_property('T')
+		ISM_phases = [0,1E3,1E4,1E7]
+		particle_ids = np.zeros(len(ISM_phases)-1, dtype='int')
+		for i in range(len(ISM_phases)-1):
+
+			mask = (T>ISM_phases[i]) & (T<ISM_phases[i+1])
+			if len(IDs[mask]) > 0:
+				particle_ids[i] = IDs[mask][0]
+		print("Particle IDs chosen (0 means no particles in that ISM phase):",particle_ids)
+	else:
+		linewidths,colors,linestyles = plt_set.setup_plot_style(len(particle_ids), style=style)
+
+	# Set up subplots based on number of parameters given
+	fig,axes,dims = plt_set.setup_figure(1, sharey=True)
+	axis = axes[0]
+
+	x_prop='grain_size'
+	plt_set.setup_axis(axis, x_prop, y_prop)
+
+	bin_nums = G.get_property('grain_bin_nums')
+	bin_slopes = G.get_property('grain_bin_slopes')
+	bin_edges = snap.Grain_Bin_Edges
+	bin_centers = snap.Grain_Bin_Centers
+
+	# default to silicates for now
+	spec_num=0
+	for j,id in enumerate(particle_ids):
+		index = np.where(IDs == id)[0][0]
+		for k in range(snap.Flag_GrainSizeBins):
+			bin_num = bin_nums[index][spec_num][k]; 
+			bin_slope = bin_slopes[index][spec_num][k]; 
+			# Since the grain bin number and slopes can be extremely large, rounding errors can cause negative values near the bin edges
+			# that seem large but are small compared to the number of grains. To avoid this when plotting we dont include the 1% around the bin edges.
+			grain_sizes = np.logspace(np.log10(bin_edges[k]*1.01),np.log10(bin_edges[k+1]*0.99))
+			if y_prop=='dn/da':
+				dist_vals = (bin_num/(bin_edges[k+1]-bin_edges[k])+bin_slope*(grain_sizes-bin_centers[k]))
+			else: 
+				dist_vals = np.power(grain_sizes,4)*(bin_num/(bin_edges[k+1]-bin_edges[k])+bin_slope*(grain_sizes-bin_centers[k]))
+
+			if k==0: label = 'id ' + str(particle_ids[j])
+			else: label = None
+
+			axis.plot(grain_sizes,dist_vals, label=label, linestyle=linestyles[j], color=colors[j], linewidth=linewidths[j], zorder=3)
 
 		
 	axis.legend(loc='best', fontsize=config.SMALL_FONT, frameon=False)