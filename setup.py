33927e346d9732a56f05bb73a2034c35a4621575
Caleb Choban <calebchoban@gmail.com>
2024-05-21 10:46:33 -0400

    Added setup.py to make this a real python package.

---
+++ setup.py
@@ -0,0 +1,82 @@
+import subprocess
+import os
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+####
+#https://stackoverflow.com/questions/19569557/pip-not-picking-up-a-custom-install-cmdclass
+#BEGIN CUSTOM INSTALL COMMANDS
+#These classes are used to hook into setup.py's install process. Depending on the context:
+#$ pip install my-package
+
+#Can yield `setup.py install`, `setup.py egg_info`, or `setup.py develop`
+
+def custom_command(prepend=''):
+    C_routine_subdir = 'src/C_routines'
+    C_routines = os.listdir(C_routine_subdir)
+    for C_routine in C_routines:
+        process = subprocess.Popen(
+            "make",
+            shell=True,
+            cwd=os.path.join(C_routine_subdir,C_routine))
+        process.wait()
+        
+class CustomInstall(install):
+    def run(self):
+        custom_command()
+        install.run(self)
+
+
+class CustomDevelop(develop):
+    def run(self):
+        custom_command()
+        develop.run(self)
+
+
+class CustomEggInfo(egg_info):
+    def run(self):
+        custom_command(prepend='../../')
+        egg_info.run(self)
+
+####  END CUSTOM INSTALL COMMANDS 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crc_scripts",
+    version="1.0",
+    author = 'Caleb Choban',
+    author_email = 'cchoban@iu.edu',
+    description="Personal python scripts for analyzing FIRE simulations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/calebchoban/crc_scripts",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[            
+          'h5py',
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'pandas',
+          'astropy',
+          'yt'
+      ],
+    include_package_data=True,
+    ## to compile C code
+    cmdclass={
+        'install': CustomInstall,
+        'develop': CustomDevelop,
+        'egg_info': CustomEggInfo},
+)
+
